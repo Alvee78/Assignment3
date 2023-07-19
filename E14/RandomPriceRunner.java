b8fa71d0bb8856724d2123f2e925fe3f4ccb7efe
Alvee <alvee.rucse@gmail.com>
2023-07-19 22:44:18 +0600

    assignment 3

---
+++ E14/RandomPriceRunner.java
@@ -0,0 +1,8 @@
+package class19.E14;
+
+public class RandomPriceRunner {
+    public static void main(String[] args) {
+        RandomPrice randomPrice = new RandomPrice();
+        System.out.printf("%.2d",randomPrice.result());
+    }
+}
